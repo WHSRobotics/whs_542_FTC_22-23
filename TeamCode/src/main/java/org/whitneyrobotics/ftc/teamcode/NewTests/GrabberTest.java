acdea3760b02eeef1b0c5b405d371e22adb4b6fe
lame <mohit_banda@myabcusd.org>
2022-10-15 01:20:14 -0700

    GrabberTest

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/NewTests/GrabberTest.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/NewTests/GrabberTest.java
@@ -1,22 +1,23 @@
 package org.whitneyrobotics.ftc.teamcode.NewTests;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import org.whitneyrobotics.ftc.teamcode.lib.util.Toggler;
 
 
 public class GrabberTest extends OpMode {
-    private Servo gate;;
+    private Servo gate;
     private Servo[] servos = new Servo[2];
     private Toggler positionTog = new Toggler(101);
-    private Toggler servoSelect = new Toggler(2);
     public boolean GrabberDrop = True;
+    private Servo ServoGrip = null;
+    private boolean GripOpen = false;
     private static Toggler ServoGateToggle = new Toggler(2);
-
+    private Toggler ArmServos;
 
     @Override
     public void init() {
-        servos[0] = hardwareMap.get(Servo.class, "servo 1");
-        servos[1] = hardwareMap.get(Servo.class, "servo 2");
+        ArmServos = hardwareMap.get(Servo.class, "servo 1");
     }
 
     @Override
@@ -26,25 +27,15 @@ public class GrabberTest extends OpMode {
             positionTog.setState(positionTog.currentState()+1);
         } else if (gamepad1.dpad_left = true) {
             positionTog.setState(positionTog.currentState()-1);
-        } else if (gamepad1.dpad_down) {
-            servoSelect.currentState(servos[1]);
-        } else if (gamepad1.dpad_up) {
-            servoSelect.currentState(servos[2]);
         }
-        telemetry.addData("Current Servo", servoSelect.currentState());
         telemetry.addData("Position", positionTog.currentState());
     }
-    private void ServoGateToggler(boolean touch) {
-        ServoGateToggle.changeState(touch);
-
-        if (ServoGateToggle.currentState() == 0) {
-            gate.setPosition(0);
-            ServoGateToggle.setState(1);
-        } else {
-            gate.setPosition(1);
-        }
-
-    }
+//    private void ServoGateToggler(boolean touch) {
+//        ServoGateToggle.changeState(touch);
+//
+//        gate.setPosition(ServoGateToggle.currentState());
+//
+//    }
     private enum GrabberStates {
         CLOSE(0),
         OPEN(1);
@@ -60,13 +51,22 @@ public class GrabberTest extends OpMode {
     }
     public void armDrop() {
         if (GrabberDrop) {
-            ServoGateToggler(true);
+            ServoGateToggle.setState(1);
             gate.setPosition(GrabberStates.CLOSE.getPosition());
         } else {
-            ServoGateToggler(false);
+            ServoGateToggle.setState(1);
             gate.setPosition(GrabberStates.OPEN.getPosition());
         }
 
+    }
+    public void GripPosition() {
+        if (GripOpen) {
+            GripOpen = false;
+            ServoGrip.setPosition(GrabberStates.CLOSE.getPosition());
+        } else {
+            GripOpen = true;
+            ServoGrip.setPosition(GrabberStates.OPEN.getPosition());
+        }
     }
 
 }
