877aaa8dade8f60960a97376c009800cd4e5cff5
Perry <perryhan0123@gmail.com>
2022-10-18 16:09:37 -0700

    Low Pass Filter + Motor PID

---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/Teaching/OpModeExTest.java
@@ -0,0 +1,2 @@
+package org.whitneyrobotics.ftc.teamcode.Teaching;public class OpModeExTest {
+}
---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/lib/filters/LowPassFilter.java
@@ -0,0 +1,2 @@
+package org.whitneyrobotics.ftc.teamcode.lib.filters;public class LowPassFilter {
+}
