934ca391e4784c50efdd2bcb21ef351bed33933c
Nairrit Sengupta <nairritsengupta@gmail.com>
2022-10-26 16:57:55 -0700

    RobotImpl Update - Auto Intake Method

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/robotImpl/WHSRobotImpl.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/robotImpl/WHSRobotImpl.java
@@ -1,13 +1,19 @@
 package org.whitneyrobotics.ftc.teamcode.robotImpl;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.whitneyrobotics.ftc.teamcode.GamepadEx.GamepadEx;
+import org.whitneyrobotics.ftc.teamcode.lib.util.SimpleTimer;
 import org.whitneyrobotics.ftc.teamcode.subsys.Drivetrains.OmniDrivetrain;
 import org.whitneyrobotics.ftc.teamcode.subsys.Grabber;
 import org.whitneyrobotics.ftc.teamcode.subsys.LinearSlides;
 
+@RequiresApi(Build.VERSION_CODES.N)
 public class WHSRobotImpl {
     OmniDrivetrain robotDriveTrain;
     LinearSlides robotLinearSlides;
@@ -16,10 +22,25 @@ public class WHSRobotImpl {
     GamepadEx gamePadOne;
     GamepadEx gamePadTwo;
 
-    public WHSRobotImpl (HardwareMap hardwareMap, GamepadEx gamepadOne, Gamepad gamepadTwo){
-        robotDriveTrain = new OmniDrivetrain(hardwareMap);
-        gamePadOne = new GamepadEx(Gamepad )
-        robotLinearSlides = new LinearSlides(hardwareMap, gamepad1.dpad_up, gamepad1.dpad_down, gamepad1.a, gamepad1.b);
+    SimpleTimer intakeAutoTimer;
 
+    public WHSRobotImpl (HardwareMap hardwareMap, GamepadEx gamepadOne){
+        robotDriveTrain = new OmniDrivetrain(hardwareMap);
+        robotLinearSlides = new LinearSlides(hardwareMap, gamepadOne);
+        robotIntake = new Grabber(hardwareMap);
+
+        robotDriveTrain.resetEncoders();
+        robotIntake.resetEncoders();
     }
+
+    public void autoGrabber(int waitTime){
+        if (robotIntake.currentState == Grabber.GrabberStates.CLOSE){
+            robotIntake.toggleState();
+            intakeAutoTimer.set(waitTime);
+        }
+        if (intakeAutoTimer.isExpired() && (robotIntake.currentState == Grabber.GrabberStates.OPEN)){
+            robotIntake.toggleState();
+        }
+    }
+
 }
