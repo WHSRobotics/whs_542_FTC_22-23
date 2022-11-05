a6a85e3a59840897de9dbb8263629352690262cf
Nairrit Sengupta <nairritsengupta@gmail.com>
2022-11-04 17:24:31 -0700

    Motion Profile Work

--- TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/MotionProfile/MotionProfile.java
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/MotionProfile/MotionProfile.java
@@ -1,8 +1,8 @@
 package org.whitneyrobotics.ftc.teamcode.MotionProfile;
 
 public abstract class MotionProfile {
-    protected double constantJerk;
-    protected double maxAcceleration;
+    protected final double CONSTANT_JERK = 0;
+    protected double maxAcceleration = 0;
     protected double usedAcceleration;
     protected double maxVelocity;
 
@@ -10,11 +10,19 @@ public abstract class MotionProfile {
     public double finalValue;
 
     public double accelTime;
+    public double accelBasedMaxVelTime;
+    public double velBasedMaxVelTime;
+
+    public double preferredTime;
+
     public double cruiseTime = 0;
 
     public MotionProfile(double initialVal, double finalValue, double usedAcceleration, double maxVelocity){
-
+        initialValue = initialVal;
+        this.finalValue = finalValue;
+        this.usedAcceleration = usedAcceleration;
+        this.maxVelocity = maxVelocity;
     }
 
-    public abstract double calculate(double time);
+    public abstract double calculate();
 }
---
+++ TeamCode/src/main/java/org/whitneyrobotics/ftc/teamcode/MotionProfile/MotionProfileConstantJerk.java
@@ -0,0 +1,35 @@
+package org.whitneyrobotics.ftc.teamcode.MotionProfile;
+
+public class MotionProfileConstantJerk extends MotionProfile {
+
+    public MotionProfileConstantJerk(double initialVal, double finalValue, double usedAcceleration, double maxVelocity) {
+        super(initialVal, finalValue, usedAcceleration, maxVelocity);
+    }
+
+    public double calculateAccelerationTime(){
+        accelBasedMaxVelTime = maxAcceleration / CONSTANT_JERK;
+        return accelBasedMaxVelTime;
+    }
+
+    public double calculateVelocityTime(){
+        velBasedMaxVelTime = Math.sqrt(maxVelocity / CONSTANT_JERK);
+        return velBasedMaxVelTime;
+    }
+
+    public void calculateUsedTime(){
+        preferredTime = Math.max(accelBasedMaxVelTime, velBasedMaxVelTime);
+    }
+
+    @Override
+    public double calculate() {
+        double valChange = Math.abs(finalValue - initialValue);
+        boolean cruiseNecessary = ((valChange / 2) - (CONSTANT_JERK * Math.pow(accelBasedMaxVelTime, 3)) - (CONSTANT_JERK * Math.pow(velBasedMaxVelTime, 3)) > valChange);
+        if (cruiseNecessary){
+            cruiseTime = (valChange - (valChange / 2) - (CONSTANT_JERK * Math.pow(accelBasedMaxVelTime, 3)) - (CONSTANT_JERK * Math.pow(velBasedMaxVelTime, 3))/(CONSTANT_JERK * Math.pow(velBasedMaxVelTime, 2)));
+        } else {
+            cruiseTime = 0;
+            preferredTime = Math.max(((-2 * maxVelocity) + ((Math.pow(maxVelocity, 2)) - (4 * maxAcceleration * -valChange))) / (4 * maxAcceleration), ((-2 * maxVelocity) + ((Math.pow(maxVelocity, 2)) - (4 * maxAcceleration * -valChange))) / (4 * maxAcceleration));
+        }
+        return Math.signum(valChange);
+    }
+}
